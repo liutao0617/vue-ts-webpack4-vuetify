0aad65a55304248e1ed6d7747c43fb79a4e1508f
刘陶 <tao.liu@geely.com>
2019-04-12 09:01:20 +0800

    修改样式，logo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <v-app>
-    <core-filter />
+    <Login />
+    <core-filter v-if="test"/>
 
-    <core-toolbar />
+    <core-toolbar v-if="test"/>
 
-    <core-drawer />
+    <core-drawer v-if="test"/>
 
-    <core-view />
+    <core-view v-if="test"/>
   </v-app>
 </template>
-<script>
-import Vue from 'vue'
-export default Vue.extend({
-})
-</script>
 
 <style lang="scss">
 @import '@/styles/index.scss';
@@ -23,3 +19,16 @@ export default Vue.extend({
   vertical-align: unset;
 }
 </style>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Login from '@/components/Login.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Login,
+  },
+})
+export default class App extends Vue {
+test:boolean = false;
+}
+</script>
